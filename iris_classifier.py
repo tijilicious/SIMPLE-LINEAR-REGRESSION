329f7998cc77e4d92e014215e62dd68174a698f9
tijilicious <74861557+tijilicious@users.noreply.github.com>
2021-01-13 16:07:09 +0530

    Update iris_classifier.py

--- iris_classifier.py
+++ iris_classifier.py
@@ -1,20 +1,28 @@
+#IMPORTING LIBRARIES
+
 from sklearn import datasets
 from sklearn.neighbors import KNeighborsClassifier
 import numpy
 iris=datasets.load_iris()
 
+
+#SETTING FEATURES AND LABELS TO CLASSIFY
 features=iris.data
 labels=iris.target
 
+
+#FITTING DATA TO KMEANS CLASSIFIER
 clf=KNeighborsClassifier()
 clf.fit(features, labels)
 
+
+#INPUT SEPAL LENGTH, SEPAL WIDTH, PETAL LENGTH AND PETAL WIDTH
 print("enter features:")
 a=input()
 b=input()
 c=input()
 d=input()
 
-
+#OUTPUT 
 preds=clf.predict([[a,b,c,d]])
 print(preds)
